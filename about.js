4bf5aa2cd3a16ac885826484267c0acc3ea05cd3
Marek C <you@example.com>
2022-05-02 21:47:46 -0400

    edits to about page

--- about.js
+++ about.js
@@ -1,9 +1,71 @@
-const downArrow = document.querySelector(".about--downArrow");
-const aboutPara = document.querySelector(".aboutParagraph");
 
-const downArrowHandler = () => {
-	downArrow.classList.toggle("open-rotate");
-	aboutPara.classList.toggle("about--hidden");
+
+// const downArrow = document.querySelector(".about--downArrow");
+// const aboutPara = document.querySelector(".aboutParagraph");
+
+// const downArrowHandler = () => {
+// 	downArrow.classList.toggle("open-rotate");
+// 	aboutPara.classList.toggle("about--hidden");
+// };
+
+// downArrow.addEventListener("click", downArrowHandler);
+
+/***** PAGE SWITCH FUNCTION - ABOUT PAGE *****/
+const buttonPastor = document.getElementById("pastor");
+const buttonChurch = document.getElementById("church--vision");
+const pastorPage = document.getElementById("aboutPastor");
+const churchPage = document.getElementById("aboutChurch");
+const togglePage = document.getElementsByClassName("pageHide");
+const activePage = document.getElementsByClassName("activeLink");
+const slideIn = document.getElementsByClassName("indexBody")
+
+const switchPage = () => {
+	// console.log('pastor button')
+	// console.log(pastorPage.classList.contains("pageHide"))
+	if (pastorPage.classList.contains("pageHide")) {
+		console.log("pastorPage");
+		churchPage.classList.add("pageHide");
+		pastorPage.classList.remove("pageHide");
+		// churchPage.classList.add("indexBody");
+		// pastorPage.classList.remove("indexBody");
+		buttonPastor.classList.add("activeLink");
+		buttonChurch.classList.remove("activeLink");
+	} else {
+		return
+	}
 };
 
-downArrow.addEventListener("click", downArrowHandler);
+const toggleChurchPage = () => {
+	if (churchPage.classList.contains("pageHide")) {
+		console.log("churchPage");
+		pastorPage.classList.add("pageHide");
+		churchPage.classList.remove("pageHide");
+		// pastorPage.classList.add("indexBody");
+		// churchPage.classList.remove("indexBody");
+		buttonChurch.classList.add("activeLink");
+		buttonPastor.classList.remove("activeLink");
+	} else {
+		return
+	}
+};
+
+buttonPastor.addEventListener("click", switchPage);
+buttonChurch.addEventListener("click", toggleChurchPage);
+
+// TOGGLE BUTTON
+
+const barOuter = document.querySelector(".bar-outer");
+const options = document.querySelectorAll(".bar-grey .option");
+let current = 1;
+options.forEach((option, i) => (option.index = i + 1));
+options.forEach(option =>
+                option.addEventListener("click", function() {
+  barOuter.className = "bar-outer";
+  barOuter.classList.add(`pos${option.index}`);
+  if (option.index > current) {
+    barOuter.classList.add("right");
+  } else if (option.index < current) {
+    barOuter.classList.add("left");
+  }
+  current = option.index;
+}));
--- index.js
+++ index.js
@@ -1,62 +1,31 @@
 /***** HAMBURGER MENU OPEN AND CLOSE *****/
-const hamburgerMenu = document.getElementById("hamburger--menu");
-const hamburgerMenuNavlinks = document.getElementById(
-	"hamburger--mobile-navLinks"
-);
+// const hamburgerMenu = document.getElementById("hamburger--menu");
+// const hamburgerMenuNavlinks = document.getElementById(
+// 	"hamburger--mobile-navLinks"
+// );
 
-const closeMenu = document.getElementById("closeMenu");
-const navLinks = document.getElementsByTagName("li");
-const otherCloseMenu = document.getElementById("closeMenu");
+// const closeMenu = document.getElementById("closeMenu");
+// const navLinks = document.getElementsByTagName("li");
+// const otherCloseMenu = document.getElementById("closeMenu");
 
-const menuOpenHandler = () => {
-	console.log("menu open");
-	hamburgerMenuNavlinks.style.visibility = "visible";
-	hamburgerMenuNavlinks.style.width = "35%";
-	hamburgerMenuNavlinks.style.opacity = "1";
-};
+// const menuOpenHandler = () => {
+// 	console.log("menu open");
+// 	hamburgerMenuNavlinks.style.visibility = "visible";
+// 	hamburgerMenuNavlinks.style.width = "35%";
+// 	hamburgerMenuNavlinks.style.opacity = "1";
+// };
 
-const menuCloseHandler = () => {
-	console.log("menu close");
-	hamburgerMenuNavlinks.style.visibility = "hidden";
-	hamburgerMenuNavlinks.style.width = "10%";
-	hamburgerMenuNavlinks.style.opacity = "0";
-	// hamburgerMenuNavlinks.style.display = "none";
-};
+// const menuCloseHandler = () => {
+// 	console.log("menu close");
+// 	hamburgerMenuNavlinks.style.visibility = "hidden";
+// 	hamburgerMenuNavlinks.style.width = "10%";
+// 	hamburgerMenuNavlinks.style.opacity = "0";
+// 	hamburgerMenuNavlinks.style.display = "none";
+// };
 
-hamburgerMenu.addEventListener("click", menuOpenHandler);
-closeMenu.addEventListener("click", menuCloseHandler);
+// hamburgerMenu.addEventListener("click", menuOpenHandler);
+// closeMenu.addEventListener("click", menuCloseHandler);
 
-/***** PAGE SWITCH FUNCTION - ABOUT PAGE *****/
-const selectorPastor = document.getElementById("pastor");
-const selectorChurch = document.getElementById("church--vision");
-const pastorPage = document.getElementById("aboutPastor");
-const churchPage = document.getElementById("aboutChurch");
-const togglePage = document.getElementsByClassName("pageHide");
-const activePage = document.getElementsByClassName("activeLink");
-
-const togglePastorPage = () => {
-	if (pastorPage.classList == "pageHide") {
-		console.log("clicked");
-		churchPage.classList.add("pageHide");
-		pastorPage.classList.remove("pageHide");
-		selectorPastor.classList.add("activeLink");
-		selectorChurch.classList.remove("activeLink");
-	} else {
-		return;
-	}
-};
-
-const toggleChurchPage = () => {
-	if (churchPage.classList == "pageHide") {
-		console.log("clicked");
-		pastorPage.classList.add("pageHide");
-		churchPage.classList.remove("pageHide");
-		selectorChurch.classList.add("activeLink");
-		selectorPastor.classList.remove("activeLink");
-	} else {
-		return;
-	}
-};
 
 // fade-in effect on scroll
 
@@ -94,6 +63,5 @@ const toggleChurchPage = () => {
 	checkPosition();
 })();
 
-selectorPastor.addEventListener("click", togglePastorPage);
-selectorChurch.addEventListener("click", toggleChurchPage);
+
 
